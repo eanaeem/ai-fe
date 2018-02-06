1eda2c45db3f231435e9693fab9d58719ab87527
eanaeem <eanaeem@gmail.com>
2018-02-06 11:05:03 +0000

    checking commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React app</h1>
-          <p> hi thi is reactive app </p>
+          <p> hi thi is first reactive </p>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
